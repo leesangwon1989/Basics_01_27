876d0b0a801dd3bd42ec8d05744f4c3eb8dda0bb
HEUKWU <yoo4068@gmail.com>
2023-01-28 20:30:21 +0900

    프로그래밍 기초 2일 : 알고리즘
    Co-authored-by: seonghyun519 <sparklep519@gmail.com>

---
+++ src/Team2/D2/yeongwoo/Main1.java
@@ -0,0 +1,11 @@
+package Team2.D2.yeongwoo;
+
+public class Main1 {
+    public int solution(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return 45 - sum;
+    }
+}
---
+++ src/Team2/D2/yeongwoo/Main3.java
@@ -0,0 +1,11 @@
+package Team2.D2.yeongwoo;
+
+public class Main3 {
+    public double solution(int[] arr) {
+        int sum = 0;
+        for (int j : arr) {
+            sum += j;
+        }
+        return (double) sum / arr.length;
+    }
+}
