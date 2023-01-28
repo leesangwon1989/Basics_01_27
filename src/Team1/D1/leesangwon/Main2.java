5496b3d6271ec4469ecd33ed2570695c5cb82b09
snagwon <ezlsw@naver.com>
2023-01-28 20:12:22 +0900

    프로그램밍 기초 1일 : 알고리즘
    수정2
    
    Co-authored-by: HEUKWU <sparklep519@gmail.com>
    Co-authored-by: seonghyun519 <yoo4068@gmail.com>
    Co-authored-by: yykim1010 <yykim1010@naver.com>

--- src/Team1/D1/leesangwon/Main2.java
+++ src/Team1/D1/leesangwon/Main2.java
@@ -2,10 +2,12 @@ package Team1.D1.leesangwon;
 
 public class Main2 {
     public static void main(String[] args) {
+        Solution22 solution22 = new Solution22();
+        System.out.println(solution22.solution(15));
     }
 }
-class Solution2 {
-    public String test(int num) {
+class Solution22 {
+    public String solution(int num) {
         String answer = "";
 
         if (num % 2 == 0) {
--- src/Team1/D1/leesangwon/Main3.java
+++ src/Team1/D1/leesangwon/Main3.java
@@ -1,12 +1,14 @@
 package Team1.D1.leesangwon;
 public class Main3 {
     public static void main(String[] args) {
+        Solution33 solution33 = new Solution33();
+        System.out.println(solution33.solution("abc"));
     }
 }
 
 
-    class Solution3 {
-    public String test(String s) {
+    class Solution33 {
+    public String solution(String s) {
         String answer = "";
         if(s.length()%2==0) {
             answer = s.substring(s.length()/2-1,s.length()/2+1);
